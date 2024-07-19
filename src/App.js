25c12b1b0d8ad19b7392c36da7aa196e1ab4bdf9
SeethaChellappan <seetha.chellappan@gmail.com>
2024-07-19 12:10:29 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-magic-numbers */
 import { React } from 'react';
 import './App.scss';
 
+const displayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const App = () =>
 	<div className="App">
-		Ready to start.
+		{ displayNumbers.map((element, index) =>
+			<div key={ index }>{ element }</div>) }
+		<div>123456789</div>
 	</div>;
 
 export default App;
